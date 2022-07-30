0195c7ff9c2e3ea128018dea9f9dcc9e46dd4317
Alex Dawson <arjunsivakumar2@gmail.com>
2022-07-30 18:37:55 +0530

    chunk init and draw

---
+++ Classes.js
@@ -0,0 +1,20 @@
+class Chunk{
+    //coordinates are in screenspace
+    constructor(x=0, y=0){
+        this.x = x
+        this.y = y
+    }
+
+    //draw this chunk to the canvas
+    draw(ctx, chunkSize, color, border){
+        //solid fill
+        ctx.fillStyle = color
+        ctx.arc(x * chunkSize, y * chunkSize, (chunkSize-2) / 2, 0, 2*Math.PI)
+        ctx.fill()
+
+        //outline
+        ctx.strokeStyle = border
+        ctx.arc(x * chunkSize, y * chunkSize, (chunkSize-2) / 2, 0, 2*Math.PI)
+        ctx.stroke()
+    }
+}
