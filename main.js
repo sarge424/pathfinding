d5aea555f25154589ed6750e7b596c8e26182164
Alex Dawson <arjunsivakumar2@gmail.com>
2022-07-30 21:54:57 +0530

    set grid size on page load

--- main.js
+++ main.js
@@ -5,12 +5,26 @@ let ctx
 let map
     
 function start(){
+    const dimensions = get_wh()
+
     canvas = document.getElementById('canvas')
-    canvas.width = 1000
-    canvas.height = 600
+    canvas.width = dimensions[0] * 25
+    canvas.height = dimensions[1] * 25
     ctx = canvas.getContext('2d')
 
     //grid takes width, height and chunkSize
-    map = new Grid(50, 30, 20)
+    map = new Grid(dimensions[0], dimensions[1], 25)
     map.drawGrid(ctx)
+}
+
+function get_wh(){
+    //gets 100% width and 80% height to get grid size
+    let container = document.getElementById('container')
+    let w = container.clientWidth
+    let h = container.clientHeight * 0.8
+
+    w = w - w % 25
+    h = h - h % 25
+
+    return[w/25, h/25]
 }
